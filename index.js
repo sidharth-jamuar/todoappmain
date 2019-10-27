980824d4e43f4eb71858d987a0c82eda27fd04e9
unknown <rahuljamuar21@gmail.com>
2019-10-27 20:47:55 +0530

    prefix client

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ require("./routes/userRoutes")(app)
 require("./routes/todoRoutes")(app)
 if (process.env.NODE_ENV === 'production') {
  
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.listen(PORT,()=>{
