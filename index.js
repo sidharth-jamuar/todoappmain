4820853dcc1886b83ba4d86cc450bae5823136bf
unknown <rahuljamuar21@gmail.com>
2019-10-29 02:24:50 +0530

    unified

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const app=require('express')();
+const express=require('express');
+const app=express();
 const bodyParser=require('body-parser');
 const mongoose=require("mongoose");
 const keys=require("./config/keys")
 const cors=require("cors");
 
 app.use(cors({origin:"http://localhost:3000"}))
+
 const PORT=process.env.PORT || 3004;
 mongoose.Promise=global.Promise;
-mongoose.connect(keys.mongoDBURI,{useNewUrlParser:true},()=>{
+mongoose.connect(keys.mongoDBURI,{useNewUrlParser:true, useUnifiedTopology: true },()=>{
   console.log("connected to database")
 })
 mongoose.set('useFindAndModify', false);
